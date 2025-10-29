a62d8cfd69487fbd5002e4f9e7175558ab6a6a09
Napat Tirmongkol <napatnom@gmail.com>
2025-10-30 03:27:45 +0700

    Update KPI colors in CSS for better visual distinction and correct stylesheet link in header

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : 'ระบบยืมคืนอุปกรณ์การแพทย์'; ?></title>
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="css/style.css"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style> /* ... (โค้ด CSS ของ header.user-info ... เหมือนเดิม) ... */
         .header .user-info {
