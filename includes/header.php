b11f29f58d1a24220521d95d060b5e79a8f86747
Napat Tirmongkol <napatnom@gmail.com>
2025-11-03 12:04:34 +0700

    css

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($page_title) ? $page_title : 'ระบบยืมคืนอุปกรณ์การแพทย์'; ?></title>
+    <title><?php echo isset($page_title) ? $page_title : 'ระบบยืมคืนอุปกรณ์'; ?></title>
     
     <script>
         (function() {
@@ -30,6 +30,7 @@
             }
         })();
     </script>
+    <link rel="icon" type="image/png" href="images/logo.png">
     <link rel="stylesheet" href="CSS/style.css?v=2.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
